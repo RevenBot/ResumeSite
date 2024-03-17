d2e063a3b211a7e59ed1e96e4151e26367df601f
Kevin Sinchi <kevinjss97@gmail.com>
2024-03-17 15:55:54 +0100

    :bricks: Store mode in LocalStorage

--- src/context/mode/store.js
+++ src/context/mode/store.js
@@ -1,10 +1,16 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-
-const useStore = create((set) => ({
-  caosMode: true,
-  switchMode: () => set((state) => ({ caosMode: !state.caosMode })),
-  updateMode: (newMode) => set(() => ({ caosMode: newMode })),
-}));
-
+const useStore = create(
+  persist(
+    (set) => ({
+      caosMode: true,
+      switchMode: () => set((state) => ({ caosMode: !state.caosMode })),
+      updateMode: (newMode) => set(() => ({ caosMode: newMode })),
+    }),
+    {
+      name: "mode",
+    },
+  ),
+);
 export default useStore;
