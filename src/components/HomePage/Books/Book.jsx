af6df44b17732cc10461fd9bb82145aba262e749
Kevin Sinchi <kevinjss97@gmail.com>
2024-12-31 18:49:26 +0100

    :wheelchair: Improve user experience

--- src/components/HomePage/Books/Book.jsx
+++ src/components/HomePage/Books/Book.jsx
@@ -1,20 +1,30 @@
 import { Text } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
+import { Select } from "@react-three/postprocessing";
+import { useState } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Book = ({ bookType, bookName, ...props }) => {
   const { scene } = useLoader(GLTFLoader, `/models/homepage/${bookType}.glb`);
+  const [hover, setHover] = useState(false);
+
   return (
-    <group {...props}>
-      <Text
-        color={"#444"}
-        position={[3, 8, 25]}
-        rotation={[0, 0, Math.PI / 2]}
-        scale={3}
-      >
-        {bookName}
-      </Text>
-      <primitive object={scene.clone()} children-0-castShadow />
+    <group
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+      {...props}
+    >
+      <Select enabled>
+        <Text
+          color={hover ? "#964B00" : "#444"}
+          position={[2.5, 10, 15]}
+          rotation={[0, 0, Math.PI / 2]}
+          scale={3}
+        >
+          {bookName}
+        </Text>
+        <primitive object={scene.clone()} children-0-castShadow />
+      </Select>
     </group>
   );
 };
